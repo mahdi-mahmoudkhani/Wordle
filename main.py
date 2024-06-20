6eb3533e18700bb723f551abc68f958efa2a1e72
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-06-21 02:07:52 +0330

    Updated main.py to its correct form of backtracking algorithm

--- main.py
+++ main.py
@@ -1,50 +1,60 @@
-allLetters = {'a': [0, 1, 2, 3], 'b': [0, 1, 2, 3], 'c': [0, 1, 2, 3], 'd': [0, 1, 2, 3], 'e': [0, 1, 2, 3], 'f': [0, 1, 2, 3], 'g': [0, 1, 2, 3], 'h': [0, 1, 2, 3], 'i': [0, 1, 2, 3], 'j': [0, 1, 2, 3], 'k': [0, 1, 2, 3], 'l': [0, 1, 2, 3], 'm': [0, 1, 2, 3], 'n': [0, 1, 2, 3], 'o': [0, 1, 2, 3], 'p': [0, 1, 2, 3], 'q': [0, 1, 2, 3], 'r': [0, 1, 2, 3], 's': [0, 1, 2, 3], 't': [0, 1, 2, 3], 'u': [0, 1, 2, 3], 'v': [0, 1, 2, 3], 'w': [0, 1, 2, 3], 'x': [0, 1, 2, 3], 'y': [0, 1, 2, 3], 'z': [0, 1, 2, 3]}
+ALHPHABET = [chr(i) for i in range(97, 123)]
+
+notInLetters = {0: list(), 1: list(), 2: list(), 3: list()}
+notInPlaces = {0: list(), 1: list(), 2: list(), 3: list()}
+guessedWord = ['0', '0', '0', '0']
+
 
 def feedbackAnalisys(guessedWord, remaningChances):
-    feedback = input("Guessed Word: "+  guessedWord + '\nRemaining Chances: ' + str(remaningChances) + "\nFeedback: " )
+    feedback = input("Guessed Word: " + ''.join(guessedWord) +
+                     '\nRemaining Chances: ' + str(remaningChances) + "\nFeedback: ")
     while len(feedback) != 4:
         feedback = input("Feedback must have 4 characters\nFeedback: ")
     if feedback == '====':
-        print("Word Guessed!")
         return True
     for index in range(len(feedback)):
         if feedback[index] == '=':
-            allLetters[guessedWord[index]] = [index]
+            value = ALHPHABET.copy()
+            value.remove(guessedWord[index])
+            notInLetters[index] = value
         elif feedback[index] == '+':
-            allLetters[guessedWord[index]].remove(index)
+            notInPlaces[index].append(guessedWord[index])
         elif feedback[index] == '-':
-            allLetters[guessedWord[index]] = [-1 for _ in range(5)]
+            for i in range(4):
+                notInLetters[i] += guessedWord[index]
         else:
             print("Invalid Feedback")
             feedbackAnalisys(guessedWord, remaningChances)
+    return False
 
-def backtrackingWordle(guessedWord):
+
+def backtrackingWordle(index):
+    global guessedWord
+    if '0' not in guessedWord:
+        return True
+    for letter in ALHPHABET:
+        if letter in guessedWord or letter in notInLetters[index] or letter in notInPlaces[index]:
+            continue
+        guessedWord[index] = letter
+        if backtrackingWordle(index + 1):
+            return True
+        guessedWord[index] = '0'
+    return False
+
+
+def display():
     remaningChances = 9
-    while (not feedbackAnalisys(guessedWord, remaningChances) and remaningChances > 0):
-        global allLetters
-        allLetters = {k: v for k, v in sorted(allLetters.items(), key=lambda item: len(item[1]))}
+    while remaningChances >= 0:
+        global guessedWord
+        guessedWord = ['0', '0', '0', '0']
+        backtrackingWordle(index=0)
+        if feedbackAnalisys(guessedWord, remaningChances):
+            print("Word Guessed!")
+            break
         remaningChances -= 1
-        guessedWord = '0123'
-        guessed = 0
-        while not guessed:
-            for letter in allLetters:
-                if guessed == 4:
-                    guessed = True
-                    break
-                if allLetters[letter] == [-1 for _ in range(5)]:
-                    continue
-                elif len(allLetters[letter]) == 1:
-                    guessedWord = guessedWord.replace(str(allLetters[letter][0]), letter, 1)
-                    guessed += 1
-                else:
-                    for int in allLetters[letter]:
-                        if guessedWord[int].isdigit():
-                            guessedWord = guessedWord.replace(str(int), letter, 1)
-                            guessed += 1
-                            break
     if remaningChances == 0:
         print("Can't guess the word")
-                    
+
+
 if __name__ == '__main__':
-    firstGuess = 'abcd'
-    backtrackingWordle(firstGuess)
+    display()
